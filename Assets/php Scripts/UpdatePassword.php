edeac7a97d29b140dc2d1281e55a477fc30bdd70
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-09-27 18:14:09 +1000

    Initial commit

---
+++ Assets/php Scripts/UpdatePassword.php
@@ -0,0 +1,29 @@
+<?php
+//Server login variables
+	$server_name = "localhost";
+	$server_username = "root";
+	$server_password = "";
+	$database_name = "nsirpg";
+	
+	$password = $_POST["username_Post"];
+	$password = $_POST["password_Post"}];
+
+//Check Connection
+	$conn = new mysqli($server_name, $server_username, $sever_password, $database_name);
+	if(!$conn)
+	{
+		die("Connection Not Successful.".mysqli_connect_error());
+	}
+//http://localhost/nsirpg/UpdatePassword.php
+	
+//Change password by creating new hash
+	$salt = "\$5\$round=5000\$"."abcdefghijklmnopqrstuvwxyz1234567890".$username"\$";
+	$hash = crypt($password, $salt);
+	
+	$updatePassword = "UPDATE users SET salt, hash = '".$hash."', '".$salt."' WHERE username = '".$username."'";
+	$updateResult = mysqli_query($conn, $updatePassword)or die("error insert failed");
+	if($updateResult)
+	{
+		echo "Password Successfully Changed";
+	}
+?>
